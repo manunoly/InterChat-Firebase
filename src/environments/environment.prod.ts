8c9807983c0c1690280bd712f66a24251f194ec5
Javier Silva <wolfblazer155@gmail.com>
2020-04-08 12:56:24 -0400

    -Feature- Agregado AngularFire 5.4.2 y variables del proyecto Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDzqzmoBL2nNZse4RFaaLybdvle348PZ4Q",
+    authDomain: "givbux-beta.firebaseapp.com",
+    databaseURL: "https://givbux-beta.firebaseio.com",
+    projectId: "givbux-beta",
+    storageBucket: "givbux-beta.appspot.com",
+    messagingSenderId: "348819315376",
+    appId: "1:348819315376:web:579256044aa51368d5b975"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDzqzmoBL2nNZse4RFaaLybdvle348PZ4Q",
+    authDomain: "givbux-beta.firebaseapp.com",
+    databaseURL: "https://givbux-beta.firebaseio.com",
+    projectId: "givbux-beta",
+    storageBucket: "givbux-beta.appspot.com",
+    messagingSenderId: "348819315376",
+    appId: "1:348819315376:web:579256044aa51368d5b975"
+  }
 };
 
 /*
