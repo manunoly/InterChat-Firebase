4c2e9c736f825e6c06998378d7f9f30f337f6006
manunoly <manunoly@gmail.com>
2020-05-26 11:36:16 -0400

    basic structure for crud user

---
+++ src/app/user-crud/user-crud.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-crud',
+  templateUrl: './user-crud.page.html',
+  styleUrls: ['./user-crud.page.scss'],
+})
+export class UserCrudPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
