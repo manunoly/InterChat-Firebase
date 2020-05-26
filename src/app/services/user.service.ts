94a5039f5146e8717f03adba821669fdc9ae4c4f
manunoly <manunoly@gmail.com>
2020-05-26 17:48:05 -0400

    user service with data by key

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,19 @@
+import { DbService } from './db.service';
 import { Injectable } from '@angular/core';
 
 import { iUser } from './../chat-list/model/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  users:iUser;
-  
-  constructor() { }
+  users: [string][] = [];
 
+  constructor(private db: DbService) {}
 
+  async getUsersByType(type: string) {
+    if (this.users && this.users[type]) return this.users[type];
+    this.users[type] = this.db.getUsersByType(type);
+    return this.users[type];
+  }
 }
